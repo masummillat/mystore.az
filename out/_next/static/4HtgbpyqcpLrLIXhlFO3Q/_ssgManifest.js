f84978f0446d5391e7e483bc4a56c82482e33c17
masum millat <masum.millat@cramstack.com>
2020-09-18 21:13:18 +0600

    delete output

--- out/_next/static/4HtgbpyqcpLrLIXhlFO3Q/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fflashdeals","\u002F","\u002Fcategory\u002F[id]","\u002Fcategory\u002F[id]\u002F[subid]\u002F[subsubid]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
